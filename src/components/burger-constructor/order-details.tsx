4671ab791a24e2543ea4d2d72433c73dfccb036b
Olga Khomiakova <kolliame@gmail.com>
2024-02-08 17:29:43 +0300

    Using useAppSelector instead of useSelector

--- src/components/burger-constructor/order-details.tsx
+++ src/components/burger-constructor/order-details.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './burger-constructor.module.css';
 import doneImage from '../../images/done.png';
-import { useSelector } from 'react-redux';
+import { useAppSelector } from '../../utils/redux-hooks';
 
 const OrderDetails: React.FC = () => {
-  const orderId = useSelector((state: any) => state.createdOrder.orderId);
+  const orderId = useAppSelector((state) => state.createdOrder.orderId);
     return (
         <div className={`${styles.order} pl-30 pr-30`}>
           <p className={`text text_type_digits-large mt-30 mb-8`}>{orderId}</p>
