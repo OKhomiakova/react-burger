1c243d59720c98f5e3e308f7a0ef4c5319856a08
Olga Khomiakova <kolliame@gmail.com>
2024-02-15 02:44:56 +0300

    Updates

--- src/utils/redux-hooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../services/types'
-
-export const useAppDispatch = () => useDispatch<AppDispatch>();
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
